1c18950a011a67873013d2458322fb11fc8644b0
Nick Sellen <git@nicksellen.co.uk>
2018-03-13 13:03:23 +0100

    Register as node, collect node info from nodes

--- hello.py
+++ hello.py
@@ -6,8 +6,6 @@ import configparser
 import influxdb
 import os
 
-# gas meter increments at 0.01 cubic meters
-
 
 def main(config_filename):
 
@@ -22,7 +20,14 @@ def main(config_filename):
   )
   influxdb_client.create_database(config.get('influxdb', 'database'))
 
-  node = uavcan.make_node(config.get('canbus', 'ifname'))
+  node_info = uavcan.protocol.GetNodeInfo.Response()
+  node_info.name = config.get('node', 'name')
+
+  node = uavcan.make_node(
+      config.get('canbus', 'ifname'),
+      node_id=config.getint('node', 'id'),
+      node_info=node_info,
+  )
 
   uavcan_types = [
       uavcan.protocol.NodeStatus,
@@ -35,18 +40,20 @@ def main(config_filename):
   for uavcan_type in uavcan_types:
     node.add_handler(
         uavcan_type,
-        write_to_influxdb,
+        handle_event,
         influxdb_client=influxdb_client,
     )
-
-  thread = threading.Thread(target=node.spin, daemon=True)
-  thread.start()
 
   node.mode = uavcan.protocol.NodeStatus().MODE_OPERATIONAL
 
   print('started')
 
-  thread.join()
+  while True:
+    try:
+      node.spin(1)
+      get_node_infos(node, influxdb_client)
+    except uavcan.UAVCANException as ex:
+      print('Node error:', ex)
 
 
 def read_config(filename):
@@ -56,11 +63,42 @@ def read_config(filename):
   return config
 
 
-def extract_fields(event):
+def receive_node_info(event, influxdb_client):
+
+  # node info comes with node status too
+
+  fields = extract_fields(event.transfer.payload.status._fields)
+  node_info = event.transfer.payload
+
+  fields.update({
+      'name': str(node_info.name),
+      'software_version.major': node_info.software_version.major,
+      'software_version.minor': node_info.software_version.minor,
+  })
+
+  influxdb_client.write_points([{
+      "measurement": 'uavcan.protocol.NodeInfo',
+      "tags": {
+          "node_id": event.transfer.source_node_id,
+      },
+      "fields": fields,
+  }])
+
+
+def get_node_infos(node, influxdb_client):
+  for node_id in [2, 3]:  # TODO: get/keep a list of node ids
+    node.request(
+        uavcan.protocol.GetNodeInfo.Request(),
+        node_id,
+        lambda event: receive_node_info(event, influxdb_client),
+    )
+
+
+def extract_fields(message_fields):
 
   fields = {}
 
-  for key, value in event.message._fields.items():
+  for key, value in message_fields.items():
     if isinstance(value, uavcan.transport.PrimitiveValue):
       fields[key] = value.value
     elif isinstance(value, uavcan.transport.ArrayValue):
@@ -70,12 +108,11 @@ def extract_fields(event):
   return fields
 
 
-def write_to_influxdb(event, influxdb_client=None):
-
+def handle_event(event, influxdb_client=None):
   if influxdb_client is None:
     raise Exception('please pass in a influxdb_client option')
 
-  fields = extract_fields(event)
+  fields = extract_fields(event.message._fields)
 
   if len(fields) > 0:
     influxdb_client.write_points([{

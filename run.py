28da45798c6aea7edb7c58b6195664583b74695d
Matthias Larisch <mail@matthias-larisch.de>
2018-03-15 17:08:03 +0100

    Use threaded influxdb writer with batch writes

--- run.py
+++ run.py
@@ -4,20 +4,35 @@ import sys
 import uavcan
 import threading
 import time
-import datetime
+from datetime import datetime
 import configparser
 import influxdb
 import os
+import queue
 from collections import defaultdict
 
 uavcan.load_dsdl(
     os.path.join(os.path.dirname(__file__), 'dsdl_files', 'homeautomation'))
 
 
+def influxdb_writer(queue, influxdb_client):
+  while True:
+    buffer = list()
+    buffer.append(queue.get())
+    while not queue.empty():
+      buffer.append(queue.get_nowait())
+      queue.task_done()
+
+    influxdb_client.write_points(buffer)
+    print('send batch of', len(buffer))
+    queue.task_done()
+    time.sleep(1)
+
 def main(config_filename, *args):
 
   node_infos = defaultdict(lambda: dict(last_seen = None, last_info = None))
   config = read_config(config_filename)
+  influxdb_queue = queue.Queue()
 
   influxdb_client = influxdb.InfluxDBClient(
       config.get('influxdb', 'host'),
@@ -49,11 +64,11 @@ def main(config_filename, *args):
     node.add_handler(
         uavcan_type,
         record_event_data,
-        influxdb_client=influxdb_client,
+        influxdb_queue=influxdb_queue,
     )
 
   def node_status_cb(event):
-    node_infos[event.transfer.source_node_id]['last_seen'] = datetime.datetime.now()
+    node_infos[event.transfer.source_node_id]['last_seen'] = datetime.now()
 
   node.add_handler(uavcan.protocol.NodeStatus, node_status_cb)
   node.add_handler(uavcan.protocol.RestartNode, restart_request)
@@ -61,13 +76,16 @@ def main(config_filename, *args):
   node.mode = uavcan.protocol.NodeStatus().MODE_OPERATIONAL
   node.health = uavcan.protocol.NodeStatus().HEALTH_OK
 
+  influxdb_thread = threading.Thread(target=influxdb_writer, kwargs={'queue': influxdb_queue, 'influxdb_client': influxdb_client})
+  influxdb_thread.start()
+
   print('started')
 
   while True:
     try:
       node.spin(1)
-      get_node_infos(node, node_infos, influxdb_client)
-      receive_node_info(config.getint('node', 'id'), node_info, influxdb_client)
+      get_node_infos(node, node_infos, influxdb_queue)
+      receive_node_info(config.getint('node', 'id'), node_info, influxdb_queue)
     except uavcan.UAVCANException as ex:
       print('Node error:', ex)
 
@@ -95,7 +113,7 @@ def read_config(filename):
   return config
 
 
-def receive_node_info(node_id, node_info, influxdb_client):
+def receive_node_info(node_id, node_info, influxdb_queue):
 
   # node info comes with node status too
 
@@ -107,31 +125,32 @@ def receive_node_info(node_id, node_info, influxdb_client):
       'software_version.minor': node_info.software_version.minor,
   })
 
-  influxdb_client.write_points([{
+  influxdb_queue.put({
       "measurement": 'uavcan.protocol.NodeInfo',
       "tags": {
           "node_id": node_id,
       },
+      "time": datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
       "fields": fields,
-  }])
+  })
 
 
-def get_node_infos(node, node_infos, influxdb_client):
+def get_node_infos(node, node_infos, influxdb_queue):
 
   def handle_event_for(node_id, event):
     if not event:
       return
 
     node_infos[node_id]['info'] = event.transfer.payload
-    node_infos[node_id]['last_info'] = datetime.datetime.now()
-    return receive_node_info(node_id, event.transfer.payload, influxdb_client),
+    node_infos[node_id]['last_info'] = datetime.now()
+    return receive_node_info(node_id, event.transfer.payload, influxdb_queue),
 
-  now = datetime.datetime.now()
+  now = datetime.now()
   for node_id, node_info in node_infos.items():
-    if not isinstance(node_info['last_seen'], datetime.datetime):
+    if not isinstance(node_info['last_seen'], datetime):
       continue
     last_seen_ago = now - node_info['last_seen']
-    last_info_ago = now - node_info['last_info'] if isinstance(node_info['last_info'], datetime.datetime) else None
+    last_info_ago = now - node_info['last_info'] if isinstance(node_info['last_info'], datetime) else None
     if last_seen_ago.total_seconds() < 5 and (last_info_ago is None or last_info_ago.total_seconds() > 3600):
       node.request(
           uavcan.protocol.GetNodeInfo.Request(),
@@ -154,20 +173,21 @@ def extract_fields(message_fields):
   return fields
 
 
-def record_event_data(event, influxdb_client=None):
-  if influxdb_client is None:
-    raise Exception('please pass in a influxdb_client option')
+def record_event_data(event, influxdb_queue=None):
+  if influxdb_queue is None:
+    raise Exception('please pass in an influxdb_queue option')
 
   fields = extract_fields(event.message._fields)
 
   if len(fields) > 0:
-    influxdb_client.write_points([{
+    influxdb_queue.put({
         "measurement": event.message._type.full_name,
         "tags": {
             "node_id": event.transfer.source_node_id,
         },
+        "time": time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime(event.transfer.ts_real)),
         "fields": fields,
-    }])
+    })
 
 
 if __name__ == '__main__':

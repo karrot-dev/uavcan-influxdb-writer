a827c0d681e7b676c423d584ce09ea4fa0b2709c
Tilmann Becker <tilmann.becker@gmail.com>
2018-04-13 19:30:57 +0200

    revert

--- run.py
+++ run.py
@@ -103,7 +103,7 @@ def main(config_filename, *args):
   def publish_current_time():
     microseconds = round(time.time() * 1000000)
     print('publishing time', microseconds)
-    Timestamp = uavcan.DATATYPES['uavcan.Timestamp']
+    Timestamp = uavcan.TYPENAMES['uavcan.Timestamp']
     msg = Timestamp(usec=microseconds)
     node.broadcast(msg)
 
